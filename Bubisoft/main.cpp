3e32db8310457f5a250cf1fd5bd24437c3a3f554
dcsepregi <dcsepregi@gmail.com>
2019-11-11 18:55:46 +0100

    server, client keretrendszer

--- Bubisoft/main.cpp
+++ Bubisoft/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 #include "Bubisoft_Net.hpp"
+#include "mainClient.h"
+#include "mainServer.h"
 //#include "MainLaci.hpp"
 
 //#include "Bubi_package.h"
@@ -14,12 +16,20 @@ using namespace std;
 
 int main()
 {
+    char tipus;
 
-        cout << "Hello!" << endl;
-       // MainLaci();
-              cout << "Bye!" << endl;
-              string c;
-              cin>>c;
+    //cout << "Hello!" << endl << endl;
+    //  MainLaci();
+    cout << "Server(s) or Client(c): ";
+    cin >> tipus;
+
+    switch(tipus) {
+        case 's' : MainServer(); break;
+        case 'c' : MainClient(); break;
+    }
+    cout << endl << "Waiting for character..." << endl;
+    string c;
+    cin>>c;
 
     return 0;
 }
